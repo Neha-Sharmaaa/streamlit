a7f0ddd7e820ea552df41d58054621cca24cf11b
Neha-Sharmaaa <neha.k@adypu.edu.in>
2026-04-06 16:46:17 +0530

    feat: add punkt_tab to NLTK resource downloads in utils.py

--- nlp_pipeline_app/utils.py
+++ nlp_pipeline_app/utils.py
@@ -11,6 +11,7 @@ import plotly.express as px
 
 # Download NLTK resources
 nltk.download('punkt', quiet=True)
+nltk.download('punkt_tab', quiet=True)
 nltk.download('stopwords', quiet=True)
 nltk.download('wordnet', quiet=True)
 nltk.download('omw-1.4', quiet=True)
